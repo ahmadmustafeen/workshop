e89c82245a1360fea6604f3d5f4d648a62d79b92
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2023-05-21 21:29:43 +0500

    FEAT: fix code structure

--- app/components/ui-components/button.js
+++ app/components/ui-components/button.js
@@ -1,13 +1,17 @@
 export default function Button(props) {
+  const { isTransparent, label } = props;
+
+  // object destructoring ES6
+
   return (
     <div
       className={`px-4 w-fit md:px-8 cursor-pointer text-center text-sm md:text-base rounded-full py-1.5 md:py-3 border-2 border-primary duration-1000 ${
-        props.isTransparent
+        isTransparent
           ? "bg-transparent hover:bg-primary hover:text-white text-primary"
           : "bg-primary text-white hover:bg-[#169670] hover:border-[#169670]"
       }`}
     >
-      <h5> {props.label} </h5>
+      <h5> {label} </h5>
     </div>
   );
 }
